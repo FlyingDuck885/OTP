ec32c43f422edbcfebdd7c7ebe47357eb104e1ca
Jeca <164709113+FlyingDuck885@users.noreply.github.com>
2025-12-29 14:27:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 const BREVO_API_KEY = process.env.BREVO_API_KEY;
 
@@ -43,3 +44,4 @@ app.post("/send-otp", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ EcoVision OTP server running on port ${PORT}`));
+
